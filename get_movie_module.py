a52a08e99dc472ff40ed992734096ee1e29e37b3
Eze Ahunanya <ezeahunanya@outlook.com>
2020-11-08 16:19:36 +0000

    feat: Update get_movie_module.py
    Now the function inside the module is more robust.  It searches for specific movie data by using the names of the data such as 'runtime' for movie times. Before data was gathered by indexing specific positions in lists which assumes the structure is the same for all websites. This was seen with marvel's the avengers movie as there is no box office figure on the website. The old function pulled an incorrect data point assuming it was the box office figure.

--- get_movie_module.py
+++ get_movie_module.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 
-
-
 def get_movie_data(urls_list):
     """Get the movie data from its url.
 
@@ -29,6 +27,7 @@ def get_movie_data(urls_list):
                 if 'root.RottenTomatoes.context.scoreInfo' in line:
                     scores_data_dict = line
                     break
+
             scores_data_dict = (scores_data_dict.replace('root.RottenTomatoes'
                                 '.context.scoreInfo = ', '').replace('true',
                                 'True').replace('false', 'False').replace('null',
@@ -38,20 +37,49 @@ def get_movie_data(urls_list):
             df = (pd.DataFrame.from_dict(eval(scores_data_dict),
                  orient='index').reset_index())
 
-            # extract other movie info and add to the dataframe
+            # extract movie title and add to dataframe
             movie_title = soup.title.text[:-len(' - Rotten Tomatoes')]
-            df['movie_title'] = pd.Series([movie_title] * len(df['index']))
-            release_date_theaters = (soup.find_all('li', class_='meta-row '
-                                    'clearfix')[6].contents[3].contents[1].text)
+            df['movie_title'] = pd.Series([movie_title] * len(df.index))
+
+            max_limit = len(soup.find_all('li', class_='meta-row clearfix'))
+
+            # extract release date and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Release Date (Theaters):' in li_descendants:
+                    release_date_theaters = li_descendants[7]
+                    break
+                else:
+                    release_date_theaters = np.nan
+
             df['release_date_theaters'] = (pd.Series([release_date_theaters]
-                                          * len(df['index'])))
-            box_office_gross_usa = soup.find_all('li', class_='meta-row '
-                                   'clearfix')[8].contents[3].text
+                                                      * len(df.index)))
+
+            # extract runtime and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Runtime:' in li_descendants:
+                    runtime = li_descendants[7].split('\n')[1]
+                    break
+                else:
+                    runtime = np.nan
+
+            df['runtime'] = pd.Series([runtime] * len(df.index))
+
+            # extract box office info and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Box Office (Gross USA):' in li_descendants:
+                    box_office_gross_usa = li_descendants[5]
+                    break
+                else:
+                    box_office_gross_usa = np.nan
+
             df['box_office_gross_usa'] = (pd.Series([box_office_gross_usa]
-                                         * len(df['index'])))
-            runtime = (soup.find_all('li', class_='meta-row clearfix')[9]
-                      .contents[3].text.split('\n')[2].replace(' ', ''))
-            df['runtime'] = pd.Series([runtime] * len(df['index']))
+                                                     * len(df.index)))
 
         else:
             # save html file in respone variable
@@ -75,20 +103,49 @@ def get_movie_data(urls_list):
             df1 = (pd.DataFrame.from_dict(eval(scores_data_dict),
                  orient='index').reset_index())
 
-            # extract other movie info and add to the dataframe
+            # extract movie title and add to dataframe
             movie_title = soup.title.text[:-len(' - Rotten Tomatoes')]
-            df1['movie_title'] = pd.Series([movie_title] * len(df1['index']))
-            release_date_theaters = (soup.find_all('li', class_='meta-row '
-                                    'clearfix')[6].contents[3].contents[1].text)
+            df1['movie_title'] = pd.Series([movie_title] * len(df1.index))
+
+            max_limit = len(soup.find_all('li', class_='meta-row clearfix'))
+
+            # extract release date and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Release Date (Theaters):' in li_descendants:
+                    release_date_theaters = li_descendants[7]
+                    break
+                else:
+                    release_date_theaters = np.nan
+
             df1['release_date_theaters'] = (pd.Series([release_date_theaters]
-                                          * len(df1['index'])))
-            box_office_gross_usa = soup.find_all('li', class_='meta-row '
-                                   'clearfix')[8].contents[3].text
+                                                      * len(df1.index)))
+
+            # extract runtime and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Runtime:' in li_descendants:
+                    runtime = li_descendants[7].split('\n')[1]
+                    break
+                else:
+                    runtime = np.nan
+
+            df1['runtime'] = pd.Series([runtime] * len(df1.index))
+
+            # extract box office info and add to dataframe
+            for i in[i for i in range(0, max_limit)]:
+                li_tag_line = soup.find_all('li', class_='meta-row clearfix')[i]
+                li_descendants = list(li_tag_line.descendants)
+                if 'Box Office (Gross USA):' in li_descendants:
+                    box_office_gross_usa = li_descendants[5]
+                    break
+                else:
+                    box_office_gross_usa = np.nan
+
             df1['box_office_gross_usa'] = (pd.Series([box_office_gross_usa]
-                                         * len(df1['index'])))
-            runtime = (soup.find_all('li', class_='meta-row clearfix')[9]
-                      .contents[3].text.split('\n')[2].replace(' ', ''))
-            df1['runtime'] = pd.Series([runtime] * len(df1['index']))
+                                                     * len(df1.index)))
 
             # combine two dataframes
             df = pd.concat([df, df1], ignore_index = 'True')
